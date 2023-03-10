efc7ce76cd1ac73e10399c0f97fefe5324c77ef6
liushihao456 <liushihao@pku.edu.cn>
2023-03-10 17:29:56 +0800

    Added supertrend cross strategy

--- demo.py
+++ demo.py
@@ -1,9 +1,68 @@
 from backtesting import Backtest, Strategy
 from backtesting.lib import crossover
+from numpy import ceil, floor
+from ta import ATRMD, SUPERTREND
+import talib
 
 from backtesting.test import SMA
 
 from data_reader import read_min_csv
+
+
+class SupertrendCross(Strategy):
+    suptertrend_atr_n = 10
+    suptertrend_atr_m = 2
+    atr_n = 14
+    atr_md_n = 14
+
+    def init(self):
+        self.initial_capital = 1000000
+        self.supertrend = self.I(
+            SUPERTREND,
+            self.data.High,
+            self.data.Low,
+            self.data.Close,
+            self.suptertrend_atr_n,
+            self.suptertrend_atr_m,
+        )
+        self.atr = self.I(
+            talib.ATR, self.data.High, self.data.Low, self.data.Close, self.atr_n
+        )
+        self.atr_md = self.I(
+            ATRMD,
+            self.data.High,
+            self.data.Low,
+            self.data.Close,
+            self.atr_n,
+            self.atr_md_n,
+        )
+
+    def next(self):
+        if crossover(self.data.Close, self.supertrend) and self.atr_md[-1] >= 7:
+            self.buy(size=int(self.initial_capital / self.data.Close[-1]))
+        elif crossover(self.supertrend, self.data.Close) and self.atr_md[-1] >= 7:
+            self.sell(size=int(self.initial_capital / self.data.Close[-1]))
+
+        if (
+            crossover(self.data.Close, self.supertrend)
+            and self.atr_md[-1] < 7
+            and self.position
+        ):
+            self.position.close()
+        elif (
+            crossover(self.supertrend, self.data.Close)
+            and self.atr_md[-1] < 7
+            and self.position
+        ):
+            self.position.close()
+
+        for trade in self.trades:
+            if trade.is_long and not trade.sl:
+                p = trade.entry_price / (1 + 0.0015)
+                trade.sl = ceil(p * 0.993 * 100) / 100
+            elif trade.is_short and not trade.sl:
+                p = trade.entry_price / (1 - 0.0015)
+                trade.sl = floor(p * 1.007 * 100) / 100
 
 
 class SmaCross(Strategy):
@@ -22,10 +81,15 @@ class SmaCross(Strategy):
 df = read_min_csv(
     "./data/300340.csv",
     "./data/300340_adj.csv",
-    freq="30min",
+    freq="15min",
     start_date="20180101",
     end_date="20221010",
 )
-bt = Backtest(df, SmaCross, commission=0.0015, exclusive_orders=True)
+# bt = Backtest(df, SmaCross, commission=0.0015, exclusive_orders=True)
+bt = Backtest(
+    df, SupertrendCross, cash=1000000, commission=0.0015, exclusive_orders=True
+)
 stats = bt.run()
-bt.plot(superimpose=False)
+print(stats)
+bt.plot(superimpose=False, resample=False)
+stats["_trades"].to_csv("result/trades.csv", index=False)
---
+++ ta.py
@@ -0,0 +1,32 @@
+import numpy as np
+import talib
+
+
+def SUPERTREND(high, low, close, n, m):
+    atr = talib.ATR(high, low, close, n)
+    is_up = close[n] > close[n - 1]
+    res = [np.nan] * (n - 1)
+    for i in range(n - 1, len(high)):
+        if is_up:
+            st = (high[i] + low[i]) / 2 - m * atr[i]
+            if not np.isnan(res[i - 1]):
+                st = max(st, res[i - 1])
+            if close[i] < st:
+                is_up = False
+                st = (high[i] + low[i]) / 2 + m * atr[i]
+            res.append(st)
+        else:
+            st = (high[i] + low[i]) / 2 + m * atr[i]
+            if not np.isnan(res[i - 1]):
+                st = min(st, res[i - 1])
+            if close[i] > st:
+                is_up = True
+                st = (high[i] + low[i]) / 2 - m * atr[i]
+            res.append(st)
+    return np.array(res)
+
+
+def ATRMD(high, low, close, n, m):
+    atr = talib.NATR(high, low, close, n)
+    sum = talib.SMA(atr, m) * m
+    return atr / sum * 100
