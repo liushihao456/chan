6bf119b8f81476a6e636470434ce69b1eac07d5c
liushihao456 <liushihao@pku.edu.cn>
2023-12-13 10:25:07 +0800

    Added random colors to indicators

--- ui/src/components/Indicators.jsx
+++ ui/src/components/Indicators.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import '../css/legends.css';
 import { read_indicator_csv } from '../data/reader';
 import { Series } from './Chart';
@@ -7,6 +7,7 @@ export const Indicators = (props) => {
     const { freq } = props;
     const [data, setData] = useState({});
     const [indicatorDisabled, setIndicatorDisabled] = useState({});
+    const colors = useRef({});
 
     useEffect(() => {
         if (!(freq in data))
@@ -17,6 +18,20 @@ export const Indicators = (props) => {
                     });
             });
     }, [freq]);
+
+    const getRandomColor = useCallback(() => {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }, []);
+
+    const getIndicatorColor = useCallback((name) => {
+        if (!(name in colors.current)) colors.current[name] = getRandomColor();
+        return colors.current[name];
+    }, []);
 
     const legendOnClick = useCallback(
         (name) => {
@@ -48,6 +63,7 @@ export const Indicators = (props) => {
             <>
                 <div className="legend-wrapper">
                     {data[freq]
+                        .filter((v) => v['value'][v['value'].length - 1]['value'] > 2)
                         .map((v) => v['name'])
                         .filter((v, i, a) => a.indexOf(v) === i)
                         .map((v, i) => (
@@ -55,25 +71,25 @@ export const Indicators = (props) => {
                                 className={'legend' + (indicatorDisabled[v] ? ' off' : '')}
                                 key={i}
                                 onClick={() => legendOnClick(v)}
+                                style={{ color: getIndicatorColor(v) }}
                             >
                                 {v}
                             </div>
                         ))}
                 </div>
-                {data[freq].map(
-                    (v, i) =>
-                        !indicatorDisabled[v['name']] &&
-                        v['value'][v['value'].length - 1]['value'] > 2 && (
-                            <Series
-                                type={'line'}
-                                data={v['value']}
-                                lastValueVisible={false}
-                                priceLineVisible={false}
-                                lineWidth={1}
-                                key={i}
-                            />
-                        )
-                )}
+                {data[freq]
+                    .filter((v) => v['value'][v['value'].length - 1]['value'] > 2 && !indicatorDisabled[v['name']])
+                    .map((v, i) => (
+                        <Series
+                            type={'line'}
+                            data={v['value']}
+                            lastValueVisible={false}
+                            priceLineVisible={false}
+                            lineWidth={1}
+                            color={getIndicatorColor(v['name'])}
+                            key={i}
+                        />
+                    ))}
             </>
         )
     );
